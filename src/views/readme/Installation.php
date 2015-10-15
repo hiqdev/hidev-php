c6f95c54cd5e32f8fac0f77b95d8836571976037
Andrii Vasyliev <sol@hiqdev.com>
2015-10-15 12:35:20 +0000

    + readme Installation section template

---
+++ src/views/readme/Installation.php
@@ -0,0 +1,21 @@
+The preferred way to install this <?= $config->package->type ?> is through [composer](http://getcomposer.org/download/).
+
+<?php if ($config->package->type === 'project') { ?>
+```
+php composer.phar create-project "<?= $config->package->fullName ?>:*" directory2install
+```
+<?php } else { ?>
+Either run
+
+```
+php composer.phar require "<?= $config->package->fullName ?>"
+```
+
+or add
+
+```json
+"<?= $config->package->fullName ?>": "*"
+```
+
+to the require section of your composer.json.
+<?php } ?>
