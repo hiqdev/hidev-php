1933bb1a9a1e2aee7938bc6c3953f632a2dd0aba
Andrii Vasyliev <sol@hiqdev.com>
2015-11-17 16:56:47 +0000

    minor

--- src/views/readme/Installation.php
+++ src/views/readme/Installation.php
@@ -1,13 +1,13 @@
 The preferred way to install this <?= $config->package->type ?> is through [composer](http://getcomposer.org/download/).
 
 <?php if ($config->package->type === 'project') { ?>
-```
+```sh
 php composer.phar create-project "<?= $config->package->fullName ?>:*" directory2install
 ```
 <?php } else { ?>
 Either run
 
-```
+```sh
 php composer.phar require "<?= $config->package->fullName ?>"
 ```
 
