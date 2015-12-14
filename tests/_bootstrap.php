09edc3dcf69cfcc9a8ec288ab05f4a69f8708696
Andrii Vasyliev <sol@hiqdev.com>
2015-12-14 18:30:48 +0000

    fixed tests

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -9,11 +9,6 @@
  * @copyright Copyright (c) 2015, HiQDev (http://hiqdev.com/)
  */
 
-error_reporting(-1);
+error_reporting(E_ALL & ~E_NOTICE);
 
-define('YII_ENABLE_ERROR_HANDLER', false);
-define('YII_DEBUG', true);
-$_SERVER['SCRIPT_NAME']     = '/' . __DIR__;
-$_SERVER['SCRIPT_FILENAME'] = __FILE__;
 require_once __DIR__ . '/../vendor/autoload.php';
-require_once __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
