dadf5b1af15defd0b6b8569da1e08bba8d50e085
Andrii Vasyliev <sol@hiqdev.com>
2016-01-06 12:39:45 +0000

    redoing with yii2-extraconfig

---
+++ src/yii2-extraconfig.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * HiDev config for PHP projects
+ *
+ * @link      https://github.com/hiqdev/hidev-php
+ * @package   hidev-php
+ * @license   BSD-3-Clause
+ * @copyright Copyright (c) 2015-2016, HiQDev (http://hiqdev.com/)
+ */
+
+return [
+    'components' => [
+        'configs' => [
+            '@hidev/php/config.yml',
+        ],
+        'views' => [
+            '@hidev/php/views',
+        ],
+    ],
+];
