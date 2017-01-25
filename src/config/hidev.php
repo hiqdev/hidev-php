bc665f685a0e9243cee76928498f07ce9d1dd11d
Andrii Vasyliev <sol@hiqdev.com>
2017-01-25 09:54:52 +0000

    csfixed

--- src/config/hidev.php
+++ src/config/hidev.php
@@ -1,13 +1,13 @@
 <?php
-
-/*
+/**
  * HiDev config for PHP projects
  *
  * @link      https://github.com/hiqdev/hidev-php
  * @package   hidev-php
  * @license   BSD-3-Clause
- * @copyright Copyright (c) 2015-2016, HiQDev (http://hiqdev.com/)
+ * @copyright Copyright (c) 2015-2017, HiQDev (http://hiqdev.com/)
  */
+
 
 return [
     'components' => [
